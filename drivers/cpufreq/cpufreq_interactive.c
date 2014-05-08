e90ce2ab99270fa88fb2ef660e4cc8142bb528e1
myfluxi <linflux@arcor.de>
2014-05-08 17:53:43 +0200

    cpufreq: interactive: Honor changed minimum frequency
    This saves a few cycles in the timer routine.
    
    Change-Id: I4c0e89094a00a0b63eae6320e6c3bdbb2d4c307e
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_interactive.c

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -406,7 +406,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 	this_hispeed_freq = max(hispeed_freq, pcpu->policy->min);
 
 	if (cpu_load >= go_hispeed_load || boosted) {
-		if (pcpu->policy->cur < hispeed_freq) {
+		if (pcpu->policy->cur < this_hispeed_freq) {
 			new_freq = this_hispeed_freq;
 		} else {
 			new_freq = choose_freq(pcpu, loadadjfreq);
@@ -418,7 +418,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 		new_freq = choose_freq(pcpu, loadadjfreq);
 	}
 
-	if (pcpu->policy->cur >= hispeed_freq &&
+	if (pcpu->policy->cur >= this_hispeed_freq &&
 	    new_freq > pcpu->policy->cur &&
 	    now - pcpu->hispeed_validate_time <
 	    freq_to_above_hispeed_delay(pcpu->policy->cur)) {
