4a583508af44c644e757c757338e69a2e1a7076c
anarkia1976 <stefano.villa1976@gmail.com>
2015-02-11 06:28:24 +0100

    cpufreq: impulse: added the new governor from @neobuddy89 kernel
    * cpufreq: impulse: Clean up
    * cpufreq: impulse: Add powersave_bias
    * cpufreq: impulse: Add go_lowspeed_load
    * cpufreq: impulse: Clean up freq selection
    * cpufreq: impulse: Skip boost if already boosted
    * cpufreq: impulse: don't skip waking up speedchange_task  if target_freq > policy->cur
    
    Signed-off-by: martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    
    Change-Id: I8f3db6a07f329d2fc2174c61990d32bc6fc291ed

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -387,6 +387,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_impulse)
 #endif
 
 
